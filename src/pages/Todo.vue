3d19d618d4d46a2ae3942ecb09ab9667ec993429
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-06-26 19:33:52 +0200

    feat: add delete Todo list item functionality

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -18,7 +18,7 @@
       -->
 
       <q-item
-        v-for="task in tasks"
+        v-for="(task, index) in tasks"
         :key="task.title"
         @click="task.done = !task.done"
         :class="{ 'done bg-blue-1' : task.done}"
@@ -34,6 +34,19 @@
         </q-item-section>
         <q-item-section>
           <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section
+          v-if="task.done"
+          side
+        >
+          <q-btn
+            @click.stop="deleteTask(index)"
+            flat
+            round
+            dense
+            color="primary"
+            icon="delete"
+          />
         </q-item-section>
       </q-item>
     </q-list>
@@ -60,6 +73,11 @@ export default {
           done: false
         },
       ]
+    }
+  },
+  methods: {
+    deleteTask (index) {
+      this.tasks.splice(index, 1)
     }
   }
 }
